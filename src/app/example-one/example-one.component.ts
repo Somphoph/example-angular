4e758d1e7b3e45028158060429e66b4be10db66f
Somphop <somphop.h@outlook.com>
2021-07-30 20:15:59 +0700

    Add dynamic multi control

--- src/app/example-one/example-one.component.ts
+++ src/app/example-one/example-one.component.ts
@@ -9,7 +9,7 @@ export class ExampleOneComponent implements OnInit {
   exampleForm1 = this.fb.group(
     {
       typeCust: '', name: ['', [Validators.required, Validators.minLength(4)]], surname: [''], citizenid: [''], juristicid: ['']
-      , aliases: this.fb.array([this.fb.control('')])
+      , aliases: this.fb.array([this.fb.group({alias : [''], desc : ['']})])
     });
   options: Array<any> = [
     {
@@ -34,6 +34,6 @@ export class ExampleOneComponent implements OnInit {
   }
 
   addAlias(): void {
-    this.aliases.push(this.fb.control(''));
+    this.aliases.push(this.fb.group({alias : [''], desc : ['']}));
   }
 }
