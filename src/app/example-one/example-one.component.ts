f80e7dcb2ef5ff12c32582f1b870a84dd758f6d4
Somphoph <somphop.homprasert@krungthai.com>
2021-07-30 18:16:17 +0700

    Add dynamic form code.

--- src/app/example-one/example-one.component.ts
+++ src/app/example-one/example-one.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-example-one',
   templateUrl: './example-one.component.html',
   styleUrls: ['./example-one.component.sass']
 })
 export class ExampleOneComponent implements OnInit {
-  exampleForm1 = this.formBuilder.group(
+  exampleForm1 = this.fb.group(
     {
       typeCust: '', name: ['', [Validators.required, Validators.minLength(4)]], surname: [''], citizenid: [''], juristicid: ['']
+      , aliases: this.fb.array([this.fb.control('')])
     });
   options: Array<any> = [
     {
@@ -20,13 +21,19 @@ export class ExampleOneComponent implements OnInit {
       label: 'นิติบุคคล'
     },
   ];
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private fb: FormBuilder) {
   }
-
+  get aliases(): FormArray {
+    return this.exampleForm1.get('aliases') as FormArray;
+  }
   ngOnInit(): void {
   }
 
   save(): void {
     console.warn(this.exampleForm1.value);
+  }
+
+  addAlias(): void {
+    this.aliases.push(this.fb.control(''));
   }
 }
